b6b3df56fa7273e89064b6d32a0718c9e58bbe09
Maria Fernanda <xasmarie@gmail.com>
2024-02-28 16:51:38 -0300

    desafio-aula4-js-att

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 
-function paridade () {
-        var num = parseInt(document.getElementById("num").value);
-        var mult = parseInt(document.getElementById("mult").value);
- 
-        if (num%mult == 0){
+
+function verificaMultiplos (num, mult) { 
+     if(num%mult == 0){
         alert(num+" é múltiplo de "+mult)
-     } 
-        else{
+    } else {
         alert(num+" não é múltiplo de "+mult)
-        } 
-
     }
+} 
+function verificaPositivos (num, mult) {
+    if(num<=0 || mult<=0) {
+     return alert('Somente números positivos')
+} 
+verificaMultiplos(num, mult)
+}
 
-    function multiplicacao () { 
-        var multiplicacao1 = parseInt(document.getElementById("multiplicacao1").value);
-        var multiplicacao2 = parseInt(document.getElementById("multiplicacao2").value);
+        function resultado() {
+            var num = parseInt(document.getElementById("num").value);
+            var mult = parseInt(document.getElementById("mult").value);
 
-        if(multiplicacao1 <= 0 || multiplicacao2<= 0) {
-         return alert('Somente numeros positivos')
-        }
-        
-        alert(multiplicacao1 * multiplicacao2)
-        
-    }
+            verificaPositivos(num, mult)
 
-    function adicao () {
-        var soma1 = parseInt(document.getElementById("soma1").value);
-        var soma2 = parseInt(document.getElementById("soma2").value);
-        
-        
-        alert(soma1 + soma2)
-    }
+   }
+
+     
+    
+
+
+
+
+
+
+
